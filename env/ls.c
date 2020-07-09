ba3ca1850102b5ae09e5605a1932680556cac6a1
wang <448110250@qq.com>
2020-07-09 17:15:23 +0800

    获取目录中的文件的信息已经实现了

--- env/ls.c
+++ env/ls.c
@@ -11,10 +11,25 @@ int main(int argc, char* argv[]) {
     /* parse arguments */
     char* path = parse_arguments(argc, argv);
     if (!success_g) return 0;
+    /* obtain pwd */
+    char cur_path[BUF_SIZE] = {0};
+    getcwd(cur_path, BUF_SIZE);
+    Log("cur_path: %s", cur_path);
     if (path != NULL) {
         /* parse path */
         Log("path: %s", path);
+        /* if start with '/', it is a absolute path, else a releative path */
+        if (path[0] == '/') {
+            strcpy(cur_path, path);
+        } else {
+            /* concat path */
+            sprintf(cur_path, "%s/%s", cur_path, path);
+        }
+        Log("after concat: %s", cur_path);
     }
+
+    /* open dir and parse files */
+    parse_dir(cur_path);
 }
 
 /* return value: if argu contions path, then return path, otherwise return null */
@@ -50,4 +65,44 @@ char* parse_arguments(int argc, char* argv[]) {
         }
     }
     return path;
+}
+
+void parse_dir(char* cur_path) {
+    DIR* dir = opendir(cur_path);
+    if (dir == NULL) {
+        success_g = false;
+        printf("open %s failed!\n", cur_path);
+        return;
+    }
+
+    struct dirent* ptr;
+    struct stat file_stat;
+    file_info_t info;
+    while ((ptr = readdir(dir)) != NULL) {
+        strcpy(info.name, ptr->d_name);
+        info.l = false;
+        if (OP_LIST) {
+            info.l = true;
+            Log("run here");
+            int ret = stat(info.name, &file_stat);
+            /* error handler */
+            if (ret == -1) {
+                success_g = false;
+                printf("parse_dir error\n");
+                return;
+            }
+            Log("run here");
+            /* get detailed info */
+            info.mode = file_stat.st_mode;
+            info.gid = file_stat.st_gid;
+            info.uid = file_stat.st_uid;
+            Log("run here");
+            info.size = file_stat.st_size;
+            info.mtim = file_stat.st_mtim;
+            Log("run here");
+            Log("mode: %x, gid: %d, uid: %d, size: %ld, name: %s", info.mode, info.gid, \
+                                                            info.uid, info.size, info.name);
+            
+        }
+    }
 }
--- env/ls.h
+++ env/ls.h
@@ -1,14 +1,17 @@
-#include <dirent.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
 #include <sys/types.h>
+#include <dirent.h>
+#include <fcntl.h>           /* Definition of AT_* constants */
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <assert.h>
 
 #include "../debug.h"
+
+#define BUF_SIZE 128
 
 struct option {
   const char* name;
@@ -26,3 +29,14 @@ bool success_g = true;
 
 /* 函数声明 */
 char* parse_arguments(int argc, char* argv[]);
+void parse_dir(char* cur_path);
+
+typedef struct file_info {
+    char name[BUF_SIZE];    /* 文件名 */
+    mode_t mode;            /* 权限 */
+    uid_t uid;                /* 所有者 */
+    gid_t gid;                /* 群组 */
+    off_t size;               /* 大小 */
+    struct timespec mtim;   /* 上次修改时间 */
+    bool l;
+} file_info_t;
